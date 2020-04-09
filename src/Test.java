a3952e6b5c6e864f4ae585bb2d6b4ee7a68d48e7
CodeWt <407191993@qq.com>
2020-04-09 15:22:02 +0800

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: wtt
+ * @Date: 20-4-9
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world ...");
+    }
+}
